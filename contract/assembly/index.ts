b6045324977a643dba3c7e69e27c8e54e63ed52e
ccddan <ccdd.dev@gmail.com>
2022-05-21 18:33:05 -0500

    chore: remove unused imports/resources

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,35 +1,30 @@
-
-import { Election, allElection } from './model';
-import { ContractPromiseBatch, context, PersistentMap } from 'near-sdk-as';
+import { Election, allElection } from "./model";
 
 export function setElection(election: Election): void {
-    let elect = allElection.get(election.id);
-    assert(elect === null, `an election with ${election.id} already exists`);
-    allElection.set(election.id, Election.fromPayload(election));
+  let elect = allElection.get(election.id);
+  assert(elect === null, `an election with ${election.id} already exists`);
+  allElection.set(election.id, Election.fromPayload(election));
 }
 
 export function getElection(id: string): Election | null {
-    return allElection.get(id);
+  return allElection.get(id);
 }
 
 export function getElections(): Election[] {
-    return allElection.values();
+  return allElection.values();
 }
 
 export function vote(id: string, candidate: string): void {
-   const election = getElection(id);
-   if (election == null) {
-       throw new Error("election not found");
-    }
-    
-    election.vote(candidate);
-    allElection.set(election.id, election);
+  const election = getElection(id);
+  if (election == null) {
+    throw new Error("election not found");
+  }
+
+  election.vote(candidate);
+  allElection.set(election.id, election);
 }
 
-export function getVotes(id: string
-    ) : Map<string, u32>  | null {
-    const election =  allElection.get(id);
-    return election? election.getVotes() : null
+export function getVotes(id: string): Map<string, u32> | null {
+  const election = allElection.get(id);
+  return election ? election.getVotes() : null;
 }
-
-
--- contract/assembly/model.ts
+++ contract/assembly/model.ts
@@ -1,58 +1,58 @@
-import { PersistentUnorderedMap, u128, context, PersistentMap } from "near-sdk-as";
+import { PersistentMap, PersistentUnorderedMap, context } from "near-sdk-as";
 
 @nearBindgen
 export class Election {
-    id: string;
-    candidates: string[];
-    scores: PersistentMap<string, u32>;
-    position: string;
-    description: string;
-    owner: string;
-    votes: u32;
-    voted: PersistentMap<string, bool>;
-    
-    public static fromPayload(payload: Election): Election {
-        const election = new Election();
-        election.id = payload.id;
-        election.candidates = payload.candidates;
-        election.position = payload.position;
-        election.owner = context.sender;
-        election.description = payload.description;
-        election.votes = 0;
-        election.scores = new PersistentMap<string, u32>(`y${allElection.length + 1}`)
-        election.voted = new PersistentMap<string, bool>(`t${allElection.length + 1}`)
-        return election;
-    }
-    public vote(candidate: string): void {
-        let votes = 0;
-        assert(!this.voted.contains(context.sender), "Already Voted")    
-        if(this.scores.contains(candidate)){
-            votes = this.scores.getSome(candidate)
-        }
+  id: string;
+  candidates: string[];
+  scores: PersistentMap<string, u32>;
+  position: string;
+  description: string;
+  owner: string;
+  votes: u32;
+  voted: PersistentMap<string, bool>;
 
-        this.scores.set(candidate, votes + 1);
-        this.votes += 1;
-        this.voted.set(context.sender, true);
-        
+  public static fromPayload(payload: Election): Election {
+    const election = new Election();
+    election.id = payload.id;
+    election.candidates = payload.candidates;
+    election.position = payload.position;
+    election.owner = context.sender;
+    election.description = payload.description;
+    election.votes = 0;
+    election.scores = new PersistentMap<string, u32>(
+      `y${allElection.length + 1}`
+    );
+    election.voted = new PersistentMap<string, bool>(
+      `t${allElection.length + 1}`
+    );
+    return election;
+  }
+  public vote(candidate: string): void {
+    let votes = 0;
+    assert(!this.voted.contains(context.sender), "Already Voted");
+    if (this.scores.contains(candidate)) {
+      votes = this.scores.getSome(candidate);
     }
 
-    public getVotes(): Map<string, u32> {
-        let scores = new Map<string, u32>()
-        for (let i = 0; i < this.candidates.length; i++) {
-            let votes = 0;
+    this.scores.set(candidate, votes + 1);
+    this.votes += 1;
+    this.voted.set(context.sender, true);
+  }
 
-            let candidate = this.candidates[i];
-            if(this.scores.contains(candidate)){
-                votes = this.scores.getSome(candidate)
-            }
-            scores.set(candidate, votes)
-        }
+  public getVotes(): Map<string, u32> {
+    let scores = new Map<string, u32>();
+    for (let i = 0; i < this.candidates.length; i++) {
+      let votes = 0;
 
-        return scores
+      let candidate = this.candidates[i];
+      if (this.scores.contains(candidate)) {
+        votes = this.scores.getSome(candidate);
+      }
+      scores.set(candidate, votes);
     }
 
-   
+    return scores;
+  }
 }
 
-let counter = 0;
 export const allElection = new PersistentUnorderedMap<string, Election>("v");
